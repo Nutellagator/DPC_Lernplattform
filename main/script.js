7d68889f43add12c88602d470f1f42b4d53011ba
Fabian <FabianHoehler@hotmail.com>
2024-05-16 15:22:32 +0200

    Erste funktionierende Version des Quizzes

--- main/script.js
+++ main/script.js
@@ -1,26 +1,36 @@
-        // der Event-Listener wartet das DOM vollständig geladen wurde, bevor code ausgeführt
-        document.addEventListener('DOMContentLoaded', function(){
-          // Referenz auf die Firebase Realtime Database erstellen
-          let database = firebase.database();
+// der Event-Listener wartet das DOM vollständig geladen wurde, bevor code ausgeführt
+document.addEventListener("DOMContentLoaded", function () {
+  // Referenz auf die Firebase Realtime Database erstellen
+  let database = firebase.database();
 
-          // Referenz auf den Pfad in der Datenbank erstellen
-          let scrumRef = database.ref('Scrum');
+  // Referenz auf den Pfad in der Datenbank erstellen
+  let scrumRef = database.ref("Scrum");
+  
+  // Daten abrufen
+  scrumRef.on("value", function (snapshot) {
+    var scrumData = snapshot.val();
 
-          // Daten abrufen
-          scrumRef.on('value', function(snapshot) {
-              var scrumData = snapshot.val();
-              //console.log(scrumData[1]["Frage"]); 
-              
-              const frage = document.createElement("h1");
-              frage.innerText = scrumData[1]["Frage"];
-              document.body.appendChild(frage);
+    let frageElement = document.getElementById("frageID");
+    frageElement.innerText = scrumData[1]["Frage"];
 
-              for (let i = 1; i <= 4; i++) {
-                const antwort = document.createElement("p")
-                antwort.innerText = scrumData[1][i];
-                document.body.appendChild(antwort);
-              }
+    for (let i = 1; i <= 4; i++) {
+      let antwortElement = document.getElementById("antwort" + i);
+      antwortElement.innerText = scrumData[1][i];
+    }
+    //Elemente werden in NodeList gespeichert
+    let antwortElemente = document.querySelectorAll(".antwort");
 
-          });
+    antwortElemente.forEach(function (antwortElement) {
+      antwortElement.addEventListener("click", function () {
+        // ID des geklickten Antwortelements erhalten und in Int ändern
+        let iD = parseInt(antwortElement.id);
+
+        console.log("Id: " + iD);
+
+        if (iD == scrumData[1]["richtig"]) {
+          alert("gg");
+        } else alert("falsch");
       });
-        
+    });
+  });
+});
